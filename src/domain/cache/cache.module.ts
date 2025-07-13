a6d02805139910fe67187f829c1a3d71f963424c
Maxim605 <komardin.macsim@gmail.com>
2025-07-13 23:33:08 +0300

    fix(codestyle): cache module moved

---
+++ src/domain/cache/cache.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { GrpcModule } from "src/grpc/grpc.module";
+import { LocalCacheService } from "./services/cache.service";
+
+@Module({
+  imports: [GrpcModule],
+  providers: [LocalCacheService],
+  controllers: [],
+  exports: [LocalCacheService],
+})
+export class CacheModule {}
