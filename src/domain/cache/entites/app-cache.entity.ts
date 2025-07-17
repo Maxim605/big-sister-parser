2bc2532b3fa485dc858f261884c736c357374598
Maxim605 <komardin.macsim@gmail.com>
2025-07-17 20:54:48 +0300

    fix(cache): code style

--- src/domain/cache/entites/app-cache.entity.ts
+++ src/domain/cache/entites/app-cache.entity.ts
@@ -1,21 +1,21 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { DateTime } from 'luxon';
+import { ApiProperty } from "@nestjs/swagger";
+import { DateTime } from "luxon";
 
 export class LocalCache {
-    // TODO
-    @ApiProperty({
-        description: 'Название модуля данные из которого в кэше',
-    })
-    @ApiProperty({
-        description: 'Тип',
-    })
-    @ApiProperty({
-        description: 'Данные',
-    })
-    @ApiProperty({
-        description: 'Дата-время создания',
-        type: 'string',
-        format: 'date-time',
-    })
-    public createdAt: DateTime;
+  // TODO
+  @ApiProperty({
+    description: "Название модуля данные из которого в кэше",
+  })
+  @ApiProperty({
+    description: "Тип",
+  })
+  @ApiProperty({
+    description: "Данные",
+  })
+  @ApiProperty({
+    description: "Дата-время создания",
+    type: "string",
+    format: "date-time",
+  })
+  public createdAt: DateTime;
 }
--- src/domain/cache/services/cache.service.ts
+++ src/domain/cache/services/cache.service.ts
@@ -1,25 +1,25 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { LocalCache } from '../entites/app-cache.entity';
-import { DateTime } from 'luxon';
+import { Injectable, Logger } from "@nestjs/common";
+import { LocalCache } from "../entites/app-cache.entity";
+import { DateTime } from "luxon";
 
 export class CachedData<T> {
-    constructor(data: T, date: DateTime) {
-        this.data = data;
-        this.date = date;
-    }
+  constructor(data: T, date: DateTime) {
+    this.data = data;
+    this.date = date;
+  }
 
-    public data: T;
-    public date: DateTime;
+  public data: T;
+  public date: DateTime;
 }
 
 // TODO
 @Injectable()
 export class LocalCacheService {
-    private readonly logger = new Logger(LocalCacheService.name);
+  private readonly logger = new Logger(LocalCacheService.name);
 
-    constructor() {}
+  constructor() {}
 
-    private generatePrimaryKey = (module: string, key: string): string => {
-        return `${module}_${key}`;
-    };
+  private generatePrimaryKey = (module: string, key: string): string => {
+    return `${module}_${key}`;
+  };
 }
