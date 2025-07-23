cb099f7552ee6142b1467c51a317ff688b9c4f85
Maxim605 <komardin.macsim@gmail.com>
2025-07-23 03:00:18 +0300

    feat(vk-parser): added friends-get parsing

---
+++ src/domain/parser/vk-parser/services/cqrs/commands/index.ts
@@ -0,0 +1,4 @@
+export * from "./load-vk-friends.service";
+export * from "./save-vk-friends.service";
+export * from "./test-save.service";
+export * from "../../../../../../thrift/services/thrift-arango.service";
